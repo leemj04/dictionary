9f318426906d5b186a0ac9bbfab01dd85d4d3c4d
lmj04 <lmj040828@gmail.com>
2023-08-25 03:48:52 +0900

    leemj04

--- app/src/main/java/com/example/dictionary/SettingFragment.java
+++ app/src/main/java/com/example/dictionary/SettingFragment.java
@@ -1,9 +1,12 @@
 package com.example.dictionary;
 
+import static android.app.Activity.RESULT_OK;
 import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +14,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -25,6 +33,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -35,6 +44,7 @@ public class SettingFragment extends Fragment {
     public FirebaseAuth firebaseAuth;
     public FragmentManager fragmentManager;
     public SelectbirthdayFragment selectbirthdayFragment;
+    public ImageView imageView;
 
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -52,6 +62,7 @@ public class SettingFragment extends Fragment {
         birthText = v.findViewById(R.id.select_birth);
         submitbtn = v.findViewById(R.id.button_submit);
         userId = v.findViewById(R.id.input_id);
+        imageView = v.findViewById(R.id.select_profile);
 
         selectbirthdayFragment.listener = new SelectbirthdayFragment.onClickListener() {
             @Override
@@ -85,8 +96,28 @@ public class SettingFragment extends Fragment {
             }
         });
 
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                launcher.launch(intent);
+            }
+        });
+
         return v;
     }
 
-
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent intent = result.getData();
+                        Uri uri = intent.getData();
+                        imageView.setImageURI(uri);
+                    }
+                }
+            });
 }
