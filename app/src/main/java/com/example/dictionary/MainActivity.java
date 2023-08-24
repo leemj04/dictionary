f9495c79365f94e67040ce7fd69949dfb47a0ac4
jisung <jisung6723@gmail.com>
2023-08-25 01:03:36 +0900

    jisung6723

--- app/src/main/java/com/example/dictionary/MainActivity.java
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.dictionary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    FragmentManager fragmentManager;
+    RecyclerviewFragment recyclerviewFragment = new RecyclerviewFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.main_bottom);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.main_frame,recyclerviewFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                fragmentManager = getSupportFragmentManager();
+
+                if(id == R.id.bottom_home){
+                    fragmentManager.beginTransaction().replace(R.id.main_frame, recyclerviewFragment)
+                            .commit();
+                } else if(id == R.id.bottom_menu){
+
+                } else if(id == R.id.bottom_setting){
+
+                } else if(id == R.id.bottom_info){
+
+                }
+
+                return true;
+            }
+        });
     }
+
 }
